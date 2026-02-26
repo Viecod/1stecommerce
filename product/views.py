0f70b5191d28437bdd2c70fd57f360b30d081b1b
Viecod <Cletusvictor001@gmail.com>
2026-02-26 12:15:46 +0100

    Initial commit

---
+++ product/views.py
@@ -0,0 +1,10 @@
+#from django.shortcuts import render
+from rest_framework import viewsets from .models import Product from .serializers import ProductSerializer
+from .permissions import IsAdminOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+ 
+# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all() serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
